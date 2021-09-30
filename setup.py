39bb9a057887019c2d424ecd81a26e82e588224a
Fayaz Mohammed <fayaz.mohammed@ext.ons.gov.uk>
2021-09-30 14:56:47 +0100

    updated setup file

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@ import sys
 
 from setuptools import setup
 
-assert sys.version_info >= (3, 6, 0), "selenium-utils_auth requires Python 3.6+"
+assert sys.version_info >= (3, 6, 0), "selenium_utils requires Python 3.6+"
 from pathlib import Path  # noqa E402
 
 CURRENT_DIR = Path(__file__).parent
 sys.path.insert(0, str(CURRENT_DIR))  # for setuptools.build_meta
 
 setup(
-    name="selenium-automation",
+    name="selenium_utils",
     description="A python library to add selenium utils to apps",
-    url="https://github.com/fayaz0211/selenium-automation",
+    url="https://github.com/fayaz0211/selenium-utils",
     license="MIT",
-    packages=["selenium-utils"],
+    packages=["selenium_utils"],
     package_dir={"": "."},
-    package_data={"selenium-utils": ["selenium_core.py"]},
+    package_data={"selenium_utils": ["selenium_core.py"]},
     python_requires=">=3.6",
     install_requires=[
         "selenium~=3.0",
